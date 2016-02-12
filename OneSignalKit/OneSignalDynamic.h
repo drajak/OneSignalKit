3e95958c5feaa3c0e8c3c8fbc792d71920b9b54a
Dipu Rajak <dipu.rajak3@gmail.com>
2016-02-12 00:35:41 -0500

    initial project checkin

---
+++ OneSignalKit/OneSignalDynamic.h
@@ -0,0 +1,21 @@
+//
+//  OneSignalDynamic.h
+//  OneSignalDynamic
+//
+//  Created by Dipu Rajak on 2/10/16.
+//  Copyright © 2016 Store Online Pvt. Ltd. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OneSignalDynamic.
+FOUNDATION_EXPORT double OneSignalDynamicVersionNumber;
+
+//! Project version string for OneSignalDynamic.
+FOUNDATION_EXPORT const unsigned char OneSignalDynamicVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+
+#import <OneSignalDynamic/OneSignal.h>
+
+
---
+++ OneSignalKit/OneSignalKit.h
@@ -0,0 +1,21 @@
+//
+//  OneSignalKit.h
+//  OneSignalKit
+//
+//  Created by Dipu Rajak on 2/12/16.
+//  Copyright © 2016 Dipu Rajak. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+//! Project version number for OneSignalKit.
+FOUNDATION_EXPORT double OneSignalKitVersionNumber;
+
+//! Project version string for OneSignalKit.
+FOUNDATION_EXPORT const unsigned char OneSignalKitVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like
+
+#import <OneSignalKit/OneSignal.h>
+
+
