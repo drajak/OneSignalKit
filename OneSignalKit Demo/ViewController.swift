d1f4554fbfb83e0005cb60a484c5bb5ce2a61096
Dipu Rajak <dipu.rajak3@gmail.com>
2016-02-12 01:51:24 -0500

    demo app

---
+++ OneSignalKit Demo/ViewController.swift
@@ -0,0 +1,25 @@
+//
+//  ViewController.swift
+//  OneSignalKit Demo
+//
+//  Created by Dipu Rajak on 2/12/16.
+//  Copyright © 2016 Dipu Rajak. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+    override func didReceiveMemoryWarning() {
+        super.didReceiveMemoryWarning()
+        // Dispose of any resources that can be recreated.
+    }
+
+
+}
+
